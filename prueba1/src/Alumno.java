216a7c015fdbbd679dfcc90e09807ab3471e5f5d
alberlymartinez <alberlymartinez@gmail.com>
2010-07-30 16:58:01 +0000

    Prueba upload

---
+++ prueba1/src/Alumno.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alberly
+ */
+public class Alumno {
+private String nombre;
+private String apellido;
+
+public Alumno()  //constructor, se inicializan los atributos de la clase. Siempre se llama igual que la clase. No devuelve nada.
+{
+    nombre = "Alberly";
+    apellido = "Martínez";
+}
+
+public void funcion()
+{
+    System.out.println("El nombre es " + nombre + " El apellido es " + apellido);
+}
+
+}
---
+++ prueba1/src/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alberly
+ */
+public class Principal {
+    public static void main(String [] args)
+    {
+    Alumno elAlumno;
+
+    elAlumno= new Alumno(); //Alumno() es la invocación del constructor.
+    elAlumno.funcion();
+
+    }
+}
+
