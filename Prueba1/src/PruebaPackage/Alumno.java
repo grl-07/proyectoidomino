89afdb691ae4453a13c19831b75574179ad6b799
danieluchin <danieluchin@95cbbb11-f478-0231-94d9-996995bcf6dd>
2010-07-30 16:52:07 +0000

    SIii

---
+++ Prueba1/src/PruebaPackage/Alumno.java
@@ -0,0 +1,25 @@
+package PruebaPackage;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author daniel
+ */
+public class Alumno extends Persona
+{
+    private String numexpediente;
+
+
+    public Alumno(String numexpediente)
+    {
+        super(nombre,apellido);
+        this.numexpediente = numexpediente;
+
+    }
+
+
+}
