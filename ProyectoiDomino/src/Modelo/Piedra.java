0663001ab012b279b5ffab9e4b2dfc0a4aeeadfb
alberlymartinez <alberlymartinez@95cbbb11-f478-0231-94d9-996995bcf6dd>
2010-08-03 07:00:24 +0000

    Clase Piedra

---
+++ ProyectoiDomino/src/Modelo/Piedra.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Modelo;
+
+/**
+ *
+ * @author Alberly
+ */
+public class Piedra {
+        private int num1;
+	private int num2;
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+
+    public Piedra(int num1, int num2){     //constructor
+        this.num1=num1;
+        this.num2=num2;
+    }
+
+	/*public void finalize() throws Throwable {
+
+	}*/
+
+}
+
