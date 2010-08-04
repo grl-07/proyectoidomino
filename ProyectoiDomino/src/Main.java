b497165345f8bcf49571f00f017717f15777b969
danieluchin <danieluchin@95cbbb11-f478-0231-94d9-996995bcf6dd>
2010-08-04 05:30:20 +0000

    Ya se reparten las fichas al jugador, maquina y pozo, pero falta validar que no se repitan.

--- ProyectoiDomino/src/Main.java
+++ ProyectoiDomino/src/Main.java
@@ -1,6 +1,7 @@
 
 import Modelo.*;
 import java.io.*;
+//import com.toedter.calendar.*;
 
 /*
  * To change this template, choose Tools | Templates
@@ -21,46 +22,65 @@ public class Main {
         ListaUsuarios listadoUsuarios;
         listadoUsuarios = new ListaUsuarios();
 
-        for (i = 0; i < 2; i++) {
+        /*for (i = 0; i < 2; i++) {
 
-            try {
-                System.out.println("Introduzca su nombre: ");
-                String nombre = br.readLine();
-                System.out.println("Introduzca su apellido: ");
-                String apellido = br.readLine();
-                System.out.println("Introduzca nombre de Usuario: ");
-                String nickname = br.readLine();
-                System.out.println("Introduzca clave secreta: ");
-                String clave = br.readLine();
-                System.out.println("Elija una imagen para su Avatar: ");
-                String avatar = br.readLine();
+        try {
+        System.out.println("Introduzca su nombre: ");
+        String nombre = br.readLine();
+        System.out.println("Introduzca su apellido: ");
+        String apellido = br.readLine();
+        System.out.println("Introduzca nombre de Usuario: ");
+        String nickname = br.readLine();
+        System.out.println("Introduzca clave secreta: ");
+        String clave = br.readLine();
+        System.out.println("Elija una imagen para su Avatar: ");
+        String avatar = br.readLine();
 
-                listadoUsuarios.agregarUsuario(new Usuario(nombre, apellido, clave, nickname, avatar, 0, 0, 0));
-            } catch (Exception e) {
-            }
+        listadoUsuarios.agregarUsuario(new Usuario(nombre, apellido, clave, nickname, avatar, 0, 0, 0, 0));
+        } catch (Exception e) {
         }
+        }*/
+
+        listadoUsuarios.agregarUsuario(new Usuario("Daniel", "Sierra", "123456", "danieluchin", "dani54", 0, 0, 0, 0));
+        listadoUsuarios.agregarUsuario(new Usuario("Alberly", "Martinez", "654321", "alberlykida12", "abe28", 0, 0, 0, 0));
+        listadoUsuarios.agregarUsuario(new Usuario("Sheryl", "Ravelo", "135790", "shishi348", "shi69", 0, 0, 0, 0));
         listadoUsuarios.mostrarUsuario();
+
+        Repartidor reparto = new Repartidor();
+
+        Jugador elJugador = new Jugador("Daniel", "Sierra", "123456", "danieluchin", "dani54", 0, 0, 0, 0);
+        elJugador.getElJugador().getPiedrasEnMano();
+
+        Maquina laMaquina = new Maquina();
+        laMaquina.getLaMaquina().getPiedrasEnMano();
+
+        Mesa laMesa = new Mesa();
+        laMesa.getElPozo();
+
+        reparto.repartirPiedras(elJugador, laMaquina, laMesa);
+
 
 
         /*LLENAR LA LISTA CON TODAS LAS PIEDRAS DEL JUEGO*/
-        i = 0;
+        /*i = 0;
         j = 0;
         ListaPiedras laListaPiedras;
         laListaPiedras = new ListaPiedras();
 
         for (i = 0; i <= 6; i++) {
 
-            for (j = i; j <= 6; j++) {
-                try {
-                    laListaPiedras.agregarPiedra(new Piedra(i, j));
-                } catch (Exception e) {
-                    /*System.out.println(e);
-                    e.printStackTrace();*/
-                }
-            }
-        }
-        laListaPiedras.imprimirListaPiedras();
+        for (j = i; j <= 6; j++) {
+        try {
+        laListaPiedras.agregarPiedra(new Piedra(i, j));
 
+        } catch (Exception e) {
+        /*System.out.println(e);
+        e.printStackTrace();*/
+        /*}
+        }
+        }
+
+        laListaPiedras.imprimirListaPiedras();*/
 
     }
 }
