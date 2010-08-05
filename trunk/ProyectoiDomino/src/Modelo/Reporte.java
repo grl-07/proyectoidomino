5b169aad7976b03cd462bbdd799cf53613da591c
sheryl.ravelo@gmail.com <sheryl.ravelo@gmail.com@95cbbb11-f478-0231-94d9-996995bcf6dd>
2010-08-05 23:10:47 +0000

    Clases Reporte y Partida

---
+++ trunk/ProyectoiDomino/src/Modelo/Reporte.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Modelo;
+
+/**
+ *
+ * @author Sheryl
+ */
+public class Reporte {
+    public Reporte(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	/**
+	 *
+	 * @param lista
+	 */
+	public void imprimirReporte(ListaUsuarios lista){
+
+	}
+
+	/**
+	 *
+	 * @param Lista
+	 */
+	public ListaUsuarios ordenarPorIngresos(ListaUsuarios Lista){
+		return null;
+	}
+
+	/**
+	 *
+	 * @param Lista
+	 */
+	public ListaUsuarios ordenPartidasCreadas(ListaUsuarios Lista){
+		return null;
+	}
+
+	/**
+	 *
+	 * @param Lista
+	 */
+	public ListaUsuarios ordenPartidasGanadas(ListaUsuarios Lista){
+		return null;
+	}
+
+	/**
+	 *
+	 * @param Lista
+	 */
+	public Usuario usuarioMayorScore(ListaUsuarios Lista){
+		return null;
+	}
+}
