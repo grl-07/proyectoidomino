d750a751915188efd970fb7ea100207486297c0f
sheryl.ravelo <sheryl.ravelo@gmail.com>
2010-08-05 23:10:47 +0000

    Clases Reporte y Partida

---
+++ ProyectoiDomino/src/Modelo/Partida.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Modelo;
+//import com.toedter.calendar.JCalendar;
+/**
+ *
+ * @author Sheryl
+ */
+public class Partida {
+    private Usuario elUsuario;
+	/*private JCalendar fechaactual;
+	private JCalendar fechaIni;*/
+	private float IDPartida;
+
+	public Partida(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void cargarPartida(){
+
+	}
+
+	public void crearPartida(){
+
+	}
+
+	public boolean guardarPartida(){
+		return false;
+	}
+
+	public void salirPartida(){
+
+	}
+}
