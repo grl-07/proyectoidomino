ec59ee2f736901628c28445870c592024b11481e
sheryl.ravelo <sheryl.ravelo@gmail.com>
2010-08-18 20:51:54 +0000

    devolver las piedras en el cliente actualizado !

--- ProyectoiDomino/src/servidor/Main.java
+++ ProyectoiDomino/src/servidor/Main.java
@@ -16,6 +16,8 @@ public class Main {
 
     public static void main(String args[]) {
 
+        System.out.println("HOLA");
+
         BufferedReader br;
         br = new BufferedReader(new InputStreamReader(System.in));
         int i = 0;
