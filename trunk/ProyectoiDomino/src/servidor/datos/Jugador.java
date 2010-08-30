cf7cf0f21ea03451a9203915c3bf713f3cde5000
danieluchin <danieluchin@95cbbb11-f478-0231-94d9-996995bcf6dd>
2010-08-30 06:33:20 +0000

    Comentados: ControlJugadores, Juego, Jugador, ListaPartidas, Maquina, ListaPiedras, FuncionGeneral

--- trunk/ProyectoiDomino/src/servidor/datos/Jugador.java
+++ trunk/ProyectoiDomino/src/servidor/datos/Jugador.java
@@ -1,41 +1,36 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package servidor.datos;
 
-//import com.toedter.calendar.JCalendar;
-
-/**
- *
- * @author Alberly
- */
 public class Jugador extends Usuario {
 
     private ControlJugadores elJugador;
 
+    /**
+     * getElJugador obtiene el jugador de la clase controladora ControlJugadores.
+     * @return retorna el jugador de tipo ControlJugadores.
+     */
     public ControlJugadores getElJugador() {
         return elJugador;
     }
 
+    /**
+     * setElJugador setea al jugador del juego actual.
+     * @param recibe un jugador de tipo ControlJugadores.
+     */
     public void setElJugador(ControlJugadores elJugador) {
         this.elJugador = elJugador;
     }
 
-  /*  public JCalendar getFechaNac() {
-        return fechaNac;
-    }
-
-    public void setFechaNac(JCalendar fechaNac) {
-        this.fechaNac = fechaNac;
-    }*/
-
-    //Constructor
+    /**
+     *Constructor
+     * @param nickname nickname del usuario
+     * @param avatar avatar del usuario
+     * @param numPartidasGan numero de partidas ganadas.
+     * @param numIngresos numero de ingresos al sistema.
+     * @param numPartCreadas
+     * @param puntaje
+     */
     public Jugador(String nickname, String avatar, int numPartidasGan, int numIngresos, int numPartCreadas, int puntaje) {
         super(nickname, avatar, numPartidasGan, numIngresos, numPartCreadas, puntaje);
         elJugador = new ControlJugadores();
     }
-    /*	public void finalize() throws Throwable {
-    super.finalize();
-    }*/
 }
--- trunk/ProyectoiDomino/src/servidor/datos/Maquina.java
+++ trunk/ProyectoiDomino/src/servidor/datos/Maquina.java
@@ -1,33 +1,29 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package servidor.datos;
 
-/**
- *
- * @author Alberly
- */
 public class Maquina {
 
     private ControlJugadores laMaquina;
 
+    /**
+     * getLaMaquina obtiene un jugador de la clase controladora ControlJugadores.
+     * @return retorna el jugador de tipo ControlJugadores.
+     */
     public ControlJugadores getLaMaquina() {
         return laMaquina;
     }
 
+    /**
+     * setLaMaquina setea al jugador2 del juego actual.
+     * @param recibe un jugador de tipo ControlJugadores.
+     */
     public void setLaMaquina(ControlJugadores laMaquina) {
         this.laMaquina = laMaquina;
     }
 
-    //Constructor
+    /**
+     * Constructor
+     */
     public Maquina() {
         laMaquina = new ControlJugadores();
-    }
-
-    /*public void finalize() throws Throwable {
-
-    }*/
-    public void realizarJugada() {
     }
 }
